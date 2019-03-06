d4c5c6f9bbddf64c4afdc8865e4085de4b6e1268
nadezhdaS <44891029+nadezhdaS@users.noreply.github.com>
2019-03-06 23:26:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+
+    let money = {1: 'P', 50: 'H', 25: 'Q', 10: 'D', 5: 'N'};
+    let result = {};
+
+    Object.keys(money).sort(function (a, b) {return b - a}).forEach(item => {
+        if (currency >= item) {
+            const num = Math.floor(currency / item);
+            currency = currency % item;
+            result[money[item]] = num;
+        }
+    });
+
+    return result;
 }
